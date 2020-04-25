fa872f10f4c412e7bf51ed450d6c9715890e4145
Florian Weber <fl-weber@t-online.de>
2020-04-25 09:19:26 +0200

    refactor: updated vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,19 +2,23 @@ module.exports = {
   title: 'aver.js',
   description: 'Official documentation for aver.js',
   themeConfig: {
+    repo: 'exreplay/aver.js',
     displayAllHeaders: true,
     sidebarDepth: 1,
     nav: [
-      { text: 'Guide', link: '/guide/' }
+      { text: 'Guide', link: '/guide/' },
+      { text: 'API', link: '/api/' },
+      { text: 'Release Notes', link: 'https://github.com/exreplay/aver.js/releases' }
     ],
-    sidebar: [
-      '/',
-      '/guide/',
-      '/guide/configuration/',
-      '/guide/vuex/',
-      '/guide/router/',
-      '/guide/meta/',
-      '/guide/plugins/'
-    ]
+    sidebar: {
+      '/guide/': [
+        '/',
+        '/guide/',
+        '/guide/vuex/',
+        '/guide/router/',
+        '/guide/meta/',
+        '/guide/plugins/'
+      ]
+    }
   }
 }
